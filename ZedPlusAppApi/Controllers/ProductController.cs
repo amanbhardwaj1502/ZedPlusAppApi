05ca6fa945a094634811361fd5b8da1df2b1352a
amanbhardwaj1502 <bhardwajaman1502@gmail.com>
2025-08-21 14:06:35 +0530

    FreshProductcontoler

--- ZedPlusAppApi/Controllers/ProductController.cs
+++ ZedPlusAppApi/Controllers/ProductController.cs
@@ -23,53 +23,7 @@ namespace ZedPlusAppApi.Controllers
                 db_zedPlusShopEntities db = new db_zedPlusShopEntities();
                 if (SubMenuId == 0 && SubCategoryId == 0 && CategoryId == 0)
                 {
-                    //var result = (from tbl in db.tblProductMasters
-                    //              join tbla in db.tblBrandMasters on tbl.BrandID equals tbla.BrandID into a
-                    //              from tbla in a.DefaultIfEmpty()
-                    //              join tblc in db.tblCategoryMasters on tbl.CategoryID equals tblc.CategoryID into c
-                    //              from tblc in c.DefaultIfEmpty() 
-                    //              join tbls in db.tblSubCategoryMasters on tbl.SubcategoryID equals tbls.SubCategoryID into s
-                    //              from tbls in s.DefaultIfEmpty() 
-                    //              join tblsm in db.tblSubMenuMasters on tbl.SubMenuID equals tblsm.ID into sm
-                    //              from tblsm in sm.DefaultIfEmpty()
-                    //              join tblst in db.tblStockEntries.Where(x => x.IsDefault == true)
-                    //              on tbl.ID equals tblst.ProductID into st
-                    //              from tblst in st.DefaultIfEmpty()
-                    //                  //join tblst in db.tblStockEntries on tbl.ID equals tblst.ProductID && tblst.IsDefault == "1" into st
-                    //                  //from tblst in st.DefaultIfEmpty()
-                    //              join tblimg in db.tblVaraintsimages on tblst.ID equals tblimg.ID into im
-                    //              from tblimg in im.DefaultIfEmpty()
-
-                    //                  //join tblj in db.ProductImages on tbl.ID equals tblj.ProductID into j
-                    //                  //from tblj in j.DefaultIfEmpty()
-                    //              select new
-                    //              {
-                    //                  tbl.ID,
-                    //                  tbl.Item_Name,
-                    //                  tblc.Category_Name,
-                    //                  tbls.SubCategory_Name,
-                    //                  tblsm.SubMenu_Name,
-                    //                  tbl.Description,
-                    //                  tbl.Status,
-                    //                  tblst.Price,
-                    //                  tblst.Stock,
-                    //                  tblst.QtyperCartoon,
-                    //                  tblst.MRP,
-                    //                  tblst.NRP,
-                    //                  tblst.DP,
-                    //                  tblst.GST,
-                    //                  tblst.HSN,
-                    //                  tblst.BARCODE,
-                    //                  tblimg.ImageUrl,
-                    //                //  tbl.MRP,
-                    //                //  tbl.NRP,
-                    //               //   tbl.BasicDiscount,
-                    //                  //tblj.ImageURL,
-                    //                  tbla.Brand_Name,
-                    //                  tbl.Date,
-                    //                  //tbl.Quantity
-                    //              }).ToList();
-
+                  
                     var result = (from tbl in db.tblProducts
                                   join tbla in db.tblBrandMasters on tbl.BrandID equals tbla.BrandID into a
                                   from tbla in a.DefaultIfEmpty()
